a63c499ef83a0dbab1bcce70a8ebb466cb658788
Cobaltinno <kowboleksa@gmail.com>
2025-05-30 20:29:04 +0300

    Fix Netlify function: use return, not res.status

--- netlify/functions/bbc-news.js
+++ netlify/functions/bbc-news.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
-  const apiKey = '4cc944cdd9704f4cad11056924e0795d'; // Keep this secret in production!
+  const apiKey = '4cc944cdd9704f4cad11056924e0795d';
   const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`);
   const data = await response.json();
   return {
