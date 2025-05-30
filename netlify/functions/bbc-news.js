d870d14bb0d5730c89053073089ee7adad0bbda6
Cobaltinno <kowboleksa@gmail.com>
2025-05-30 20:27:40 +0300

    Add Netlify function for BBC News 2

--- netlify/functions/bbc-news.js
+++ netlify/functions/bbc-news.js
@@ -1,6 +1,11 @@
-export default async (req, res) => {
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
   const apiKey = '4cc944cdd9704f4cad11056924e0795d'; // Keep this secret in production!
   const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`);
   const data = await response.json();
-  res.status(200).json(data);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
 };
